c19b7ebbbbbbe04b46899d1ef836137018ed3a5e
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2023-01-05 15:29:33 -0600

    rework base Vector2 methods

--- src/heat/core/MVector2.hx
+++ src/heat/core/MVector2.hx
@@ -7,34 +7,53 @@ import haxe.ds.Vector;
 **/
 abstract MVector2<T>(Vector<T>) from Vector<T> to Vector<T> {
     public var x(get, set):T;
-    public var y(get, set):T;
-
-    public inline function new(?x:T, ?y:T) {
-        this = new Vector<T>(2);
-        init(x, y);
-    }
-
-    public inline function init(?x:T, ?y:T):MVector2<T> {
-        if (x != null) this[0] = x;
-        if (y != null) this[1] = y;
-        return this;
-    }
-
     public inline function get_x():T {
         return this[0];
     }
-
-    public inline function get_y():T {
-        return this[1];
-    }
-
     public inline function set_x(x:T):T {
         this[0] = x;
         return x;
     }
 
+    public var y(get, set):T;
+    public inline function get_y():T {
+        return this[1];
+    }
     public inline function set_y(y:T):T {
         this[1] = y;
         return y;
+    }
+
+    public inline function new(x:T, y:T) {
+        this = new Vector<T>(2);
+        init(x, y);
+    }
+
+    public inline function init(x:T, y:T):MVector2<T> {
+        this[0] = x;
+        this[1] = y;
+        return this;
+    }
+
+    public inline function initFrom(source:MVector2<T>):MVector2<T> {
+        init(source.x, source.y);
+        return this;
+    }
+
+    public inline function toImmutable():Vector2<T> {
+        return new Vector2<T>(x, y);
+    }
+
+    public static inline function fromImmutable<T>(immutable:Vector2<T>):MVector2<T> {
+        return new MVector2<T>(immutable.x, immutable.y);
+    }
+
+    public inline function clone():MVector2<T> {
+        return new MVector2<T>(x, y);
+    }
+
+    public inline function applyTo(target:MVector2<T>):MVector2<T> {
+        target.init(x, y);
+        return this;
     }
 }
--- src/heat/core/Vector2.hx
+++ src/heat/core/Vector2.hx
@@ -9,4 +9,16 @@ class Vector2<T> {
         this.x = x;
         this.y = y;
     }
+
+    public function toMutable():MVector2<T> {
+        return new MVector2<T>(x, y);
+    }
+
+    public static function fromMutable<T>(mutable:MVector2<T>):Vector2<T> {
+        return new Vector2<T>(mutable.x, mutable.y);
+    }
+
+    public function clone():Vector2<T> {
+        return new Vector2(x, y);
+    }
 }
