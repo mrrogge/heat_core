2b05e68f1d98415583182d79a35c45c91e8dbeb2
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2023-01-05 16:36:14 -0600

    Change x/y to final (guarantees immutability at compile time)

--- src/heat/core/Vector2.hx
+++ src/heat/core/Vector2.hx
@@ -2,8 +2,8 @@ package heat.core;
 
 /**An immutable vector with 2 components.**/
 class Vector2<T> {
-    public var x(default, null):T;
-    public var y(default, null):T;
+    public final x:T;
+    public final y:T;
 
     public function new(x:T, y:T) {
         this.x = x;
