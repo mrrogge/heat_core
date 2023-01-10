69c542d030ea10c2cd89738943c5827b02196b61
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2023-01-10 16:50:19 -0600

    add tests for mutable/immutable operator results

--- test/Main.hx
+++ test/Main.hx
@@ -1,8 +1,31 @@
 using buddy.Should;
 using Lambda;
 
+import heat.core.*;
+
 class Main extends buddy.SingleSuite {
     public function new() {
-        var t = new heat.core.Tuple2<String, Int>('',  2);
+        describe("For float vectors: ", {
+            it("Adding mutable plus immutable returns mutable (following LHS)", {
+                var mutable = new MVectorFloat2();
+                var immutable = new VectorFloat2();
+                (mutable + immutable).should.beType(MVector2);
+            });
+            it("Adding immutable plus mutable returns immutable (following LHS)", {
+                var immutable = new VectorFloat2();
+                var mutable = new MVectorFloat2();
+                (immutable + mutable).should.beType(Vector2);
+            });
+            it("Subtracting immutable from mutable returns mutable (following LHS)", {
+                var mutable = new MVectorFloat2();
+                var immutable = new VectorFloat2();
+                (mutable - immutable).should.beType(MVector2);
+            });
+            it("Subtracting mutable from immutable returns immutable (following LHS)", {
+                var immutable = new VectorFloat2();
+                var mutable = new MVectorFloat2();
+                (immutable - mutable).should.beType(Vector2);
+            });
+        });
     };
 }
