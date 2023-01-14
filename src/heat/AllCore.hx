dcdd198f3cd919d3cce8eee09fee4c739171b358
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2023-01-13 19:15:09 -0600

    Add AllCore

---
+++ src/heat/AllCore.hx
@@ -0,0 +1,14 @@
+package heat;
+
+typedef ILineSegment = heat.core.ILineSegment;
+typedef IVector2<T> = heat.core.IVector2<T>;
+typedef LineSegment = heat.core.LineSegment;
+typedef MathTools = heat.core.MathTools;
+typedef MLineSegment = heat.core.MLineSegment;
+typedef MVector2<T> = heat.core.MVector2<T>;
+typedef MVectorBool2 = heat.core.MVectorBool2;
+typedef MVectorFloat2 = heat.core.MVectorFloat2;
+typedef Pool<T> = heat.core.Pool<T>;
+typedef Vector2<T> = heat.core.Vector2<T>;
+typedef VectorBool2 = heat.core.VectorBool2;
+typedef VectorFloat2 = heat.core.VectorFloat2;
