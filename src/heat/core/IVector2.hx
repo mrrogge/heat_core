e3ff0fe1de4b9aa5743ab87df90354dbbe90f708
Matt Rogge <5900418+mrrogge@users.noreply.github.com>
2023-01-30 21:45:29 -0600

    add compare methods to interface

--- src/heat/core/IVector2.hx
+++ src/heat/core/IVector2.hx
@@ -6,4 +6,6 @@ package heat.core;
 interface IVector2<T> {
     public var x(get, never):T;
     public var y(get, never):T;
+    public function isSameAs(other:IVector2<T>):Bool;
+    public function isSameByPartsWith(other:IVector2<T>):IVector2<Bool>;
 }
